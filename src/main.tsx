2ed1841f699fb62e3a0a60e29ee1f74d56372c57
turnosmebna <turnosmebna@gmail.com>
2026-09-23 12:04:54 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import AgendaMEBNA from './AgendaMebna.jsx' // Traemos el nuevo sistema
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AgendaMEBNA />
   </StrictMode>,
 )
